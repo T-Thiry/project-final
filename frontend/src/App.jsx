31050c726fb44ffc296447eb1c0945d05031b289
Tavan Thiry <tavanthiry@Arilds-Mac-mini.local>
2025-08-15 12:26:01 +0200

    Add components and pages folder. Import pages in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
+import Landing from './components/pages/Landing';
+import Signup from './components/pages/Signup';
+import Login from './components/pages/Login';
+import Dashboard from './components/pages/Dashboard';
 
 export const App = () => {
 
