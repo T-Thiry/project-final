0b184df069e0a80daa77fc0e43605bb566310a33
Tavan Thiry <tavanthiry@Arilds-Mac-mini.local>
2025-08-15 12:01:34 +0200

    Import browserRouter and wrap app in BrowserRouter in main.jsx and define routes in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+
 export const App = () => {
 
   return (
-    <>
-      <h1>Welcome to Final Project!</h1>
-    </>
+    <Routes>
+      <Route path='/' element={<Signup />}></Route>
+      <Route path='/signup' element={<Signup />}></Route>
+      <Route path='/login' element={<Signup />}></Route>
+      <Route path='/dashboard' element={<Signup />}></Route>
+    </Routes>
   );
 };
+
+export default App;
