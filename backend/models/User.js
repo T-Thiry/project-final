53c77eb6eef31ce58a6e8b1cab43dc4ecf6ab433
Tavan Thiry <tavanthiry@Arilds-Mac-mini.local>
2025-08-18 16:11:31 +0200

    Add User model with schema and accessToken

---
+++ backend/models/User.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose'
+import crypto from 'crypto'
+
+
+const userSchema = new mongoose.Schema({
+ name: {
+   type: String,
+   required: true,
+ },
+ email: {
+   type: String,
+   required: true,
+   unique: true
+ },
+ password: {
+   type: String,
+   required: true,
+ },
+ accessToken: {
+   type: String,
+   default: () => crypto.randomBytes(128).toString('hex')
+ }
+});
+
+
+export const User = mongoose.model("User", userSchema);
