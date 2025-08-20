d74e263be0a4cceb5fd09fe879e414a7d2ff645f
Tavan Thiry <tavanthiry@Arilds-Mac-mini.local>
2025-08-20 13:13:55 +0200

    Add express-list-endpoints in package.json, import listendpoints and endpoints of the API in server.js, add authmiddleware.js and jsonwebtoken

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,27 +1,38 @@
-import express from 'express'
-import bcrypt from 'bcryptjs'
-import { User } from '../models/User.js'
+import express from 'express';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken'; 
+import { User } from '../models/User.js';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Load environment variables
+console.log('JWT_SECRET:', process.env.JWT_SECRET);
 
 const router = express.Router()
-
+const JWT_SECRET = process.env.JWT_SECRET || 'your_secure_secret_key'; // Use environment variable for the secret key
 
 // POST - Register a new user
 router.post("/signup", async (req, res) => {
   try {
     const { name, email, password } = req.body
     const salt = bcrypt.genSaltSync()
-    const user = new User({ name, email, password: bcrypt.hashSync(password, salt)
+    const hashedPassword = bcrypt.hashSync(password, salt);
+    const user = new User({ name, email, password: hashedPassword
     }) 
  
     //await to not send response before database finished saving
     await user.save()
+
+    // Generate a JWT token
+    const token = jwt.sign({ id: user._id, email: user.email }, JWT_SECRET, {
+    expiresIn: '1h', // Token expires in 1 hour
+    });
  
     res.status(201).json({
       success: true,
       message: "User created successfully.",
       response: {
         id: user._id,
-        accessToken: user.accessToken
+        accessToken: token,
       }
     })
  
--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,10 @@ import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
+import listEndpoints from "express-list-endpoints";
 
-dotenv.config(); 
+dotenv.config();
+console.log('JWT_SECRET:', process.env.JWT_SECRET); 
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/final-project";
 mongoose.connect(mongoUrl);
@@ -16,6 +18,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// endpoint for documentation of the API
+app.get("/", (req, res) => {
+  const endpoints = listEndpoints(app)
+  res.json({
+    message: "Welcome to StoryBudget API",
+    endpoints: endpoints
+  })
+})
+
+//endpoint routes
 app.use('/users', userRoutes);
 
 app.get("/", (req, res) => {
