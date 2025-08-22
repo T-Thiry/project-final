6d0d5f4412e45fe2539a320938a10a34cb780bc5
Tavan Thiry <tavanthiry@Arilds-Mac-mini.local>
2025-08-22 19:35:46 +0200

    Change and add additional styling to landing page

--- frontend/src/components/pages/Landing.jsx
+++ frontend/src/components/pages/Landing.jsx
@@ -4,12 +4,15 @@ import Mascot from '../../assets/images/Mascot.svg';
 
 const LandingContainer = styled.div`
   background-color: ${({ theme }) => theme.colors.primary};
-  color: ${({ theme }) => theme.colors.white}; 
-  font-family: ${({ theme }) => theme.typography.fontFamily}; 
-  height: 100vh;
+  color: ${({ theme }) => theme.colors.white};
+  font-family: ${({ theme }) => theme.typography.fontFamily};
   display: flex;
   flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
   position: relative;
+  overflow: hidden; /* Ensure stars don't overflow */
 `;
 
 const Navbar = styled.nav`
@@ -17,10 +20,11 @@ const Navbar = styled.nav`
   justify-content: space-between;
   align-items: center;
   padding: ${({ theme }) => theme.spacing(4)};
-  border-bottom: 1px solid ${({ theme }) => theme.colors.greyLight}; // Light grey border for navbar
+  border-bottom: 1px solid ${({ theme }) => theme.colors.greyLight}; 
   background-color: ${({ theme }) => theme.colors.primary};
   width: 100%;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  z-index: 3;
 `;
 
 const Logo = styled.h1`
@@ -50,80 +54,6 @@ const Button = styled.button`
 
   &:hover {
     background-color: ${({ theme }) => theme.colors.limeGreenLight};
-  }
-`;
-
-const Content = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-start;
-  text-align: center;
-  padding: ${({ theme }) => theme.spacing(4)};
-  margin-bottom: ${({ theme }) => theme.spacing(8)}; // Push the content up
-  margin-top: ${({ theme }) => theme.spacing(28)}; // Add space below the navbar
-  z-index: 2; // Ensure content is above the falling star
-`;
-
-const Title = styled.h2`
-  font-size: 2rem;
-  font-weight: ${({ theme }) => theme.typography.fontWeightBold};
-  margin-bottom: ${({ theme }) => theme.spacing(4)};
-`;
-
-const Text = styled.p`
-  font-size: 1rem;
-  font-weight: ${({ theme }) => theme.typography.fontWeightRegular};
-  margin-bottom: ${({ theme }) => theme.spacing(4)};
-`;
-
-const LimeButton = styled(Button)`
-  margin-top: ${({ theme }) => theme.spacing(4)};
-  padding: ${({ theme }) => theme.spacing(3)} ${({ theme }) => theme.spacing(6)};
-  font-size: 1rem;
-  background-color: ${({ theme }) => theme.colors.limeGreen};
-  color: ${({ theme }) => theme.colors.black};
-`;
-
-const MascotContainer = styled.div`
-  position: absolute;
-  bottom: 0;
-  left: 50%;
-  transform: translateX(-50%);
-  width: 100vw;
-  height: calc(100vw / 2);
-  display: flex;
-  justify-content: center;
-  align-items: flex-end;
-  z-index: 1;
-`;
-
-const MascotImage = styled.img`
-  position: fixed;
-  bottom: 25%; 
-  left: 50%;
-  transform: translate(-50%, 0) rotate(45deg);
-  width: 180px; // Adjust mascot size
-  height: auto;
-  animation: float 12s ease-in-out infinite;
-
-  @keyframes float {
-    0% {
-      transform: translate(-50%, 0) rotate(30deg) translateY(0); // Start position
-    }
-    25% {
-      transform: translate(-50%, -5px) rotate(32deg) translateY(-5px); // Slight upward movement and rotation
-    }
-    50% {
-      transform: translate(-50%, 0) rotate(30deg) translateY(0); // Float higher
-    }
-    75% {
-      transform: translate(-50%, 5px) rotate(26deg) translateY(-5px); // Slight downward movement and rotation
-    }
-    100% {
-      transform: translate(-50%, 0) rotate(30deg) translateY(0); // Return to start
-    }
   }
 `;
 
@@ -208,6 +138,90 @@ const FallingStar = styled.div`
   }
 `;
 
+const Title = styled.h2`
+  font-size: 2rem;
+  font-weight: ${({ theme }) => theme.typography.fontWeightBold};
+  margin-bottom: ${({ theme }) => theme.spacing(4)};
+
+   @media (min-width: 768px) {
+    font-size: 2.5rem;
+  }
+`;
+
+const Text = styled.p`
+  font-size: 1rem;
+  font-weight: ${({ theme }) => theme.typography.fontWeightRegular};
+  margin-bottom: ${({ theme }) => theme.spacing(4)};
+`;
+
+const LimeButton = styled(Button)`
+  margin-top: ${({ theme }) => theme.spacing(4)};
+  padding: ${({ theme }) => theme.spacing(3)} ${({ theme }) => theme.spacing(6)};
+  font-size: 1rem;
+  background-color: ${({ theme }) => theme.colors.limeGreen};
+  color: ${({ theme }) => theme.colors.black};
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.limeGreenLight};
+  }
+`;
+
+const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100%;
+  position: relative;
+  z-index: 2; // Ensure content is above stars
+  `;
+
+const Section = styled.section`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  text-align: center;
+  padding: ${({ theme }) => theme.spacing(8)};
+
+  @media (min-width: 768px) {
+    padding: ${({ theme }) => theme.spacing(12)};
+  }
+`;
+
+const MascotSection = styled(Section)`
+  flex: 1;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const MascotImage = styled.img`
+  width: 180px; // Adjust mascot size
+  height: auto;
+  animation: float 12s ease-in-out infinite;
+
+  @keyframes float {
+    0% {
+      transform:  translateY(0); // Start position
+    }
+    25% {
+      transform:  translateY(-10px); // Slight upward movement and rotation
+    }
+    50% {
+      transform: translateY(0); // Float higher
+    }
+    75% {
+      transform: translateY(-10px); // Slight downward movement and rotation
+    }
+    100% {
+      transform: translateY(0); // Return to start
+    }
+  }
+`;
+
 
 const Landing = () => {
   const navigate = useNavigate();
@@ -244,14 +258,16 @@ const Landing = () => {
         ))}
         <FallingStar /> {/* Single falling star */}
       </StarsContainer>
-      <Content>
-        <Title>Welcome To StoryBudget</Title>
-        <Text>Manage your budgets effortlessly.</Text>
-        <LimeButton onClick={() => navigate('/signup')}>Get Started</LimeButton>
-      </Content>
-      <MascotContainer>
-        <MascotImage src={Mascot} alt="StoryBudget Mascot" />
-      </MascotContainer>
+      <ContentContainer>
+        <Section>
+          <Title>Welcome To StoryBudget</Title>
+          <Text>Manage your budgets effortlessly.</Text>
+          <LimeButton onClick={() => navigate('/signup')}>Get Started</LimeButton>
+        </Section>
+        <MascotSection>
+          <MascotImage src={Mascot} alt="StoryBudget Mascot" />
+        </MascotSection>
+      </ContentContainer>
     </LandingContainer>
   )
 };
