654c8e32236beca4b31619dfa7d1e32afcbeb1e1
Tavan Thiry <tavanthiry@Arilds-Mac-mini.local>
2025-08-24 21:24:07 +0200

    Add Navbar and and handlelogout to Dashboard.jsx

--- frontend/src/components/pages/Dashboard.jsx
+++ frontend/src/components/pages/Dashboard.jsx
@@ -1,5 +1,64 @@
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Navbar = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: ${({ theme }) => theme.spacing(4)};
+  border-bottom: 1px solid ${({ theme }) => theme.colors.greyLight};
+  background-color: ${({ theme }) => theme.colors.primary};
+  width: 100%;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+`;
+
+const Logo = styled.h1`
+  font-size: 1rem;
+  font-weight: ${({ theme }) => theme.typography.fontWeightBold};
+  color: ${({ theme }) => theme.colors.white};
+  margin: 0;
+`;
+
+const NavButtons = styled.div`
+  display: flex;
+  gap: ${({ theme }) => theme.spacing(4)};
+
+  @media (max-width: 767px) {
+    display: none;
+  }
+`;
+
+const Button = styled.button`
+  background-color: ${({ theme }) => theme.colors.pink};
+  color: ${({ theme }) => theme.colors.black};
+  padding: ${({ theme }) => theme.spacing(2)} ${({ theme }) => theme.spacing(4)};
+  font-size: ${({ theme }) => theme.typography.fontSize};
+  border: none;
+  border-radius: ${({ theme }) => theme.spacing(2)};
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.limeGreenLight};
+  }
+`;
+
 const Dashboard = () => {
-  return <h1>Dashboard Page</h1>;
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem('accessToken'); // Example: Clear the access token
+    navigate('/'); // Redirect to the landing page
+  };
+
+  return (
+    <>
+      <Navbar>
+        <Logo>StoryBudget</Logo>
+        <NavButtons>
+          <Button onClick={handleLogout}>Logout</Button>
+        </NavButtons>
+      </Navbar>
+    </>
+  );
 };
 
 export default Dashboard;
